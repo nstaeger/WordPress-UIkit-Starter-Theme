f30f5b5f3d805b6a5d0b5d1a341f138f51e6317b
Nicolai Stäger <mail@nstaeger.de>
2014-10-19 19:46:52 +0200

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -4,9 +4,8 @@
  *
  * If no active widgets are in the sidebar, hide it completely.
  *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @author nstaeger
+ * @since 2014-08-31
  */
 ?>
 
