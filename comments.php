c39f167ba7a4ec508174bfaadb00dca5b6f6e142
nstaeger <mail@nstaeger.de>
2014-10-31 19:13:15 +0100

    Styling for comment-form
    Started, not finished yet.

--- comments.php
+++ comments.php
@@ -14,6 +14,39 @@ if (post_password_required()) {
 	return;
 }
 
+$commenter = wp_get_current_commenter();
+$req = get_option( 'require_name_email' );
+$aria_req = ( $req ? " aria-required='true'" : '' );
+
+$form_args = array(
+
+    'fields' => apply_filters('comment_form_default_fields', array(
+
+        'author' =>
+            '<div class="comment-form-author uk-form-row">'
+            . '<label for="author" class="uk-form-label">'
+                . __( 'Name', 'domainreference' )
+                . ( $req ? ' <span class="required">*</span>' : '' )
+            . '</label>'
+            . '<div class="uk-form-controls">'
+                . '<input id="author" class="uk-width-1-1" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />'
+            . '</div>'
+            . '</div>',
+
+        'email' =>
+            '<p class="comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' .
+            ( $req ? '<span class="required">*</span>' : '' ) .
+            '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+            '" size="30"' . $aria_req . ' /></p>',
+
+        'url' =>
+            '<p class="comment-form-url"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+            '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+            '" size="30" /></p>',
+
+    ))
+);
+
 ?>
 
 <section id="comments" class="comments-area">
@@ -54,11 +87,12 @@ if (post_password_required()) {
         <?php endif; ?>
 
         <?php if ( ! comments_open() ) : ?>
-            <p class="no-comments"><?php _e( 'Comments are closed.', 'twentyfourteen' ); ?></p>
+            <p class="no-comments"><?php echo 'Comments are closed.'; ?></p>
         <?php endif; ?>
 
     <?php endif; ?>
 
-    <?php comment_form(); ?>
+    <?php // TODO eventuell hier mit einem div.uk-form wrappen, anstatt das styling zu vererben ?>
+    <?php comment_form($form_args); ?>
 
 </section>
