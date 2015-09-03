900a986161d3f258021d03ae14fac51d8f6c33d4
nstaeger <mail@nstaeger.de>
2015-09-03 21:25:06 +0200

    some localization

--- 404.php
+++ 404.php
@@ -1,20 +1,19 @@
 <?php
 /**
  * 404 template
- *
- * @author nstaeger
- * @since 2014-08-31
  */
 
 get_header(); ?>
 
-<section class="uk-height-1-1 uk-vertical-align uk-text-center">
-    <div class="uk-vertical-align-middle">
-    	<h1>404</h1>
-    	<p>Sorry, but we could not find, what you were looking for.</p>
-    	<p><a href="<?php echo esc_url( home_url() ); ?>"><i class="uk-icon-reply"></i> go back to website</a></p>
-    </div>
-</section>
+    <section class="uk-height-1-1 uk-vertical-align uk-text-center">
+        <div class="uk-vertical-align-middle">
+            <h1>404</h1>
+            <p><?= __('Sorry, but we could not find, what you were looking for.', 'uikit-starter') ?></p>
+            <p>
+                <a href="<?php echo esc_url(home_url()); ?>"><i class="uk-icon-reply"></i> <?= __('go back to website', 'uikit-starter') ?>'</a>
+            </p>
+        </div>
+    </section>
 
 <?php
 
