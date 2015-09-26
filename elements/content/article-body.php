4c55ae253970782349855cc3e5252c983d095409
nstaeger <mail@nstaeger.de>
2015-09-26 18:09:52 +0200

    updated article meta style

--- elements/content/article-body.php
+++ elements/content/article-body.php
@@ -1,3 +1,9 @@
+<?php
+
+$iconClass = 'tm-icon' . (get_post_format() ? ' tm-icon-' . get_post_format() : '');
+
+?>
+
 <div class="uk-container uk-container-center">
 
     <?php
@@ -9,24 +15,32 @@
     ?>
     <p class="uk-article-meta">
         <?php printf(
-            '<span class="nst-author uk-link-reset"><a href="%1$s" rel="author"><i class="uk-icon-user"></i> %2$s</a></span>',
+            '<span class="uk-link-reset"><a href="%1$s" rel="author"><i class="uk-icon-user"></i> %2$s</a></span>',
             esc_url(get_author_posts_url(get_the_author_meta('ID'))),
             get_the_author()
         ); ?>
         <?php printf(
-            '<span class="nst-entry-time uk-margin-small-left"><i class="uk-icon-clock-o"></i> <time datetime="%1$s">%2$s</time></span>',
+            '<span class="uk-margin-small-left"><i class="uk-icon-clock-o"></i> <time datetime="%1$s">%2$s</time></span>',
             esc_attr(get_the_date('c')),
             esc_html(get_the_date())
         ); ?>
-        <span class="nst-category-list uk-margin-small-left uk-link-reset">
-            <i class="uk-icon-ticket"></i> <?php echo get_the_category_list(', '); ?>
-        </span>
         <?php if (!post_password_required() && (comments_open() || get_comments_number())) : ?>
-            <span class="nst-comments uk-margin-small-left uk-link-reset">
+            <span class="uk-margin-small-left uk-link-reset">
                 <?php comments_popup_link('<i class="uk-icon-comment"></i> ' . __('Leave a comment', 'uikit-starter'), '<i class="uk-icon-comment"></i> 1', '<i class="uk-icon-comment"></i> %'); ?>
             </span>
         <?php endif; ?>
         <?php edit_post_link('<i class="uk-icon-edit"></i> Edit', '<span class="nst-edit-link uk-margin-small-left uk-link-reset">', '</span>'); ?>
+
+        <br>
+        <span class="uk-link-reset">
+            <i class="uk-icon-ticket"></i> <?php echo get_the_category_list(', '); ?>
+        </span>
+        <span class="uk-margin-small-left uk-link-reset">
+            <a href="<?= get_post_format_link(get_post_format()) ?>">
+                <i class="<?= $iconClass ?>"></i> <?php echo get_post_format_string(get_post_format()); ?>
+            </a>
+        </span>
+
         <?php if (is_single()) : ?>
             <br/ >
             <?php the_tags('<span class="nst-tag-list uk-link-reset"><i class="uk-icon-tag"></i> ', ', ', '</span>'); ?>
